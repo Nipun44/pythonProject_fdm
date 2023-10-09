2910364bd03bb83f6efc3321b5e501b32055723b
Nipun Ganepola <85781012+Nipun44@users.noreply.github.com>
2023-10-09 15:40:19 +0530

    change archi

--- main.py
+++ main.py
@@ -1,3 +1,9 @@
 # from asgiref import simple_server
 from flask import Flask, request, render_template, Response
 from flask_cors import CORS, cross_origin
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello_world():
+    return "<p>Hello, World!</p>"
